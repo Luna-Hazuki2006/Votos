9ae644f886fcb281c89535fd0595d61e03a0dbbd
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-08-24 17:14:36 -0400

    con python

---
+++ index.js
@@ -0,0 +1,15 @@
+function revisar() {
+    const forma = document.querySelector('form')
+    const titulo = document.querySelector('#titulo')
+    if (localStorage.getItem('usuario')) {
+
+    }
+}
+
+function iniciar_sesion() {
+    
+}
+
+function registrar_usuario() {
+    
+}
