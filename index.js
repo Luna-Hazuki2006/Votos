466eb17504bcb5af0a4a8d6525ff54d13b15ed7f
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-08-24 17:57:24 -0400

    cositas

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-function revisar() {
-    const forma = document.querySelector('form')
-    const titulo = document.querySelector('#titulo')
-    if (localStorage.getItem('usuario')) {
-
-    }
-}
+// function revisar() {
+//     const forma = document.querySelector('form')
+//     const titulo = document.getElementById('titulo')
+//     if (localStorage.getItem('usuario')) {
+//         titulo.innerText = ''
+//     }
+// }
+const actual = document.getElementById('usuario')
 
 function iniciar_sesion() {
-    
+    const forma = document.querySelector('form')
+
+    const data = new FormData(forma)
+    const cedula = data.get('cedula')
+    const clave = data.get('contraseña')
+    const real = JSON.parse(localStorage.getItem(cedula))
+    if (real.clave == clave) {
+        localStorage.setItem('actual', JSON.stringify(real))
+    }
+    actual.innerText = 'Usuario: ' + cedula
 }
 
 function registrar_usuario() {
-    
+    const forma = document.querySelector('form')
+
+    const data = new FormData(forma)
+    const cedula = data.get('cedula')
+    const nombre = data.get('nombre')
+    const clave = data.get('contraseña')
+    const repetida = data.get('repetida')
+    const correo = data.get('correo')
+    console.log(data);
+    if (clave != repetida) {
+        alert('Las contraseñas no son iguales')
+        return
+    }
+    const usuario = {
+        'cedula': cedula, 
+        'nombre': nombre, 
+        'clave': clave, 
+        'correo': correo
+    }
+    localStorage.setItem(cedula, JSON.stringify(usuario))
 }
