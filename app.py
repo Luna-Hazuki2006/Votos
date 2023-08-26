665acd403c6e5b445f5d288a92101baa5cfd87e0
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-08-25 20:48:24 -0400

    mas cositas

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
 from db import candidatos, votantes
-
+# korean queen tokyo walmart 2 DRIP > TOKYO , rope SKYPE _ 4 & korean XBOX
 app = Flask(__name__, template_folder='templates')
+app.config['SECRET_KEY'] = 'kqtw2D>T,rS_4&kX'
+
