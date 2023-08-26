12c0d1d427b91b6e640ce87dfbcf8cf61e0d42f7
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-08-25 20:38:16 -0400

    con muchas cosas de python

---
+++ db.py
@@ -0,0 +1,8 @@
+import pymongo
+
+cliente = pymongo.MongoClient('mongodb+srv://lunahazuki2006:cXU0lYhSncWZ12FM@cluster0.owjghpf.mongodb.net/')
+
+db = cliente.votaciones
+
+votantes = db.votantes
+candidatos = db.candidatos
