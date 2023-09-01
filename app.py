c65d770ed5fd0e8090453c3f5da505fba859851e
Luna Hazuki <LunaHazuki2006@outlook.com>
2023-08-31 21:54:57 -0400

    funciona

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash
-from flask_login import *
+from flask_login import LoginManager
+from werkzeug.security import check_password_hash, generate_password_hash
 from db import candidatos, votantes
 from validaciones import agregar_votante, agregar_candidato
 from pprint import pprint
@@ -9,6 +10,19 @@ app.config['SECRET_KEY'] = 'kqtw2D>T,rS_4&kX'
 
 @app.route('/')
 def iniciar():
+    with open('datos_importantes', 'w+') as datos:
+        datos.write('Votantes:\n')
+        for esto in votantes.find({'estatus': 'A'}): 
+            datos.write(esto['cedula'])
+            datos.write('\n')
+            datos.write(generate_password_hash(esto['clave']))
+            datos.write('\n')
+        datos.write('Candidatos:\n')
+        for esto in candidatos.find({'estatus': 'A'}): 
+            datos.write(esto['cedula'])
+            datos.write('\n')
+            datos.write(generate_password_hash(esto['clave']))
+            datos.write('\n')
     return render_template('/principio/index.html')
 
 @app.route('/candidatos', methods=['GET'])
@@ -20,37 +34,40 @@ def listar_candidatos():
 def registrar_usuario(): 
     if request.method == 'POST':
         forma = request.form
-        nuevo_usuario = {
-            'cedula': forma['cedula'], 
-            'nombre': forma['nombre'], 
-            'apellido': forma['apellido'], 
-            'clave': forma['contraseña'], 
-            'correo': forma['correo'], 
-            'estatus': 'A'
-        }
-        if forma['tipo'] == 'votante': 
-            if agregar_votante(nuevo_usuario): 
-                id = votantes.insert_one(nuevo_usuario).inserted_id
-                if id: 
-                    flash('Se ha registrado como votante éxitosamente')
+        if forma['contraseña'] == forma['repetida']: 
+            nuevo_usuario = {
+                'cedula': forma['cedula'], 
+                'nombre': forma['nombre'], 
+                'apellido': forma['apellido'], 
+                'clave': generate_password_hash(forma['contraseña']), 
+                'correo': forma['correo'], 
+                'estatus': 'A'
+            }
+            if forma['tipo'] == 'votante': 
+                if agregar_votante(nuevo_usuario): 
+                    id = votantes.insert_one(nuevo_usuario).inserted_id
+                    if id: 
+                        flash('Se ha registrado como votante éxitosamente')
+                    else: 
+                        flash('Ha sucedido un error al registrarse')
                 else: 
-                    flash('Ha sucedido un error al registrarse')
-            else: 
-                flash('La cédula que ha registrado ya existe')
-        elif forma['tipo'] == 'candidato': 
-            if agregar_candidato(nuevo_usuario): 
-                id = candidatos.insert_one(nuevo_usuario).inserted_id
-                if id: 
-                    flash('Se ha registrado como candidato éxitosamente')
+                    flash('La cédula que ha registrado ya existe')
+            elif forma['tipo'] == 'candidato': 
+                if agregar_candidato(nuevo_usuario): 
+                    id = candidatos.insert_one(nuevo_usuario).inserted_id
+                    if id: 
+                        flash('Se ha registrado como candidato éxitosamente')
+                    else: 
+                        flash('Ha sucedido un error al registrarse')
                 else: 
-                    flash('Ha sucedido un error al registrarse')
-            else: 
-                flash('La cédula que ha registrado ya existe')
-        pprint(nuevo_usuario)
-        pprint(forma['tipo'])
+                    flash('La cédula que ha registrado ya existe')
+            pprint(nuevo_usuario)
+            pprint(forma['tipo'])
+        else: 
+            flash('Las contraseñas no son iguales')
     return render_template('/registro/index.html')
 
-@app.route('/inicio', methods=['GET'])
+@app.route('/inicio', methods=['GET', 'POST'])
 def iniciar_sesion():
     return render_template('/inicio/index.html')
 
